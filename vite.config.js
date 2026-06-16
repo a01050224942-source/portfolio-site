9c23cddb9658643d3a26ca56d56b28402955cea7
a01050224942-source <a01050224942@gmail.com>
2026-06-17 08:05:44 +0900

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "/portfolio-site/",
   plugins: [react()],
+   base: "/portfolio-site/",
   server: {
     proxy: {
       // 프론트엔드의 /api 요청을 감지하면 백엔드 8080 포트로 강제 우회 포워딩
